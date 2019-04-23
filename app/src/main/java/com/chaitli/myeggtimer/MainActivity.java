76cde48cd68a443491438f2e0946f28ab2962abc
chaitli <chaitlikalkamba@gmail.com>
2019-04-23 15:45:16 +0530

    first commit

---
+++ app/src/main/java/com/chaitli/myeggtimer/MainActivity.java
@@ -0,0 +1,47 @@
+package com.chaitli.myeggtimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar timerseekbar = findViewById(R.id.timerseekBar);
+        final TextView timerTextview = findViewById(R.id.timertextview);
+
+        timerseekbar.setMax(600);
+        timerseekbar.setProgress(1500);
+
+        timerseekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress/60;
+                int seconds = progress - (minutes * 60);
+
+                String SecondString = Integer.toString(seconds);
+
+                if(SecondString.equals("0")){
+                    SecondString = "00";
+                }
+
+                timerTextview.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
